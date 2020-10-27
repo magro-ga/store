f27b368ed46450bba5547c1561487d54739054f7
gamagro <gabriel.carvalho@youse.com.br>
2020-10-27 12:30:02 -0300

    fixed bug departments

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
   belongs_to :department, optional: true
 
-  validates :price, presence: true
+  validates :price, :department, presence: true
   validates :name, length: { minimum: 4 }
 end
